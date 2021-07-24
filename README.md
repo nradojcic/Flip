e53d94bff0946f53d1b9d7968113cdcf2e96204e
nradojcic <63752654+nradojcic@users.noreply.github.com>
2021-07-23 22:06:47 -0400

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# JS-Site-Template
-Site Template for JS Projects
+# FLIP - a Coding Project
+
+- Reverse a String, or There and Back Again.
+- A JavaScript coding exercise using arrays.
+- Write a program that displays a string in reverse.
+
+### It demonstrates:
+
+- CSS and Bootstrap Responsive Layout
+- JavaScript Fundamentals
+- JavaScript Loops
+- JavaScript Functions
+- JavaScript DOM Manipulation
+- JavaScript If/Then/Else
+- JavaScript Boolean Logic
--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-// Call Hello World
-function HelloWorld() {
-  alert("Hello World");
+// get the string from the page
+// Controller funcyion
+function getValue() {
+  // hide the alert box at the beginning of the program
+  document.getElementById("alert").classList.add("invisible");
+
+  let userString = document.getElementById("userString").value;
+
+  let revString = reverseString(userString);
+
+  displayString(revString);
+}
+
+// reverse the string
+// Logic function
+function reverseString(userString) {
+  let revString = [];
+
+  // reverse the string using a for loop, backwards
+  for (let i = userString.length - 1; i >= 0; i--) {
+    revString += userString[i];
+  }
+
+  return revString;
+}
+
+// display the reversed string to the user
+// View function
+function displayString(revString) {
+  // write to the page
+  document.getElementById(
+    "msg"
+  ).innerHTML = `Your string reversed is: <strong>${revString}</strong>`;
+
+  // show the alert box
+  document.getElementById("alert").classList.remove("invisible");
 }
